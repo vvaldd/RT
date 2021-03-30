390d601e903b6987c684c09cb845c43920d713df
vvaldd <wroztorguev@gmail.com>
2021-03-31 00:14:02 +0300

    1

--- okten/src/App.js
+++ okten/src/App.js
@@ -1,107 +1,66 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import './App.css';
-import { AutoCard } from "./componets/AutoCard/AutoCard"
+
+const Tabs = ({ tabs, selectedTab }) => {
+  return(
+    <div style={{
+      display: 'flex'
+      }}>
+      {
+      tabs.map(tab => 
+      <button style={{flex: 1, height: '50px',  background: selectedTab === tab.title ? 'yellow' : 'white'}}
+      onClick={tab.clickHHandler}>{tab.title}
+      </button>
+      )
+      }
+    
+    </div>
+  )
+}
+const PostList = ({posts}) => {
+  return (
+    <>
+    {posts.map(post=>(
+      <>
+        <h2>{post.title}</h2>
+        <p>{post.body}</p>
+      </>
+    ))}
+    </>
+  )
+}
 
 
-let cars = [
-  {id: 252, model : "auto", age  : 2000, power : 1000, color : "red"},
-  {id: 842, model : "auto2", age  : 2002, power : 1500, color : "blue"}, 
-  {id: 826, model : "auto3", age  : 2004, power : 1600, color : "green"},
-  {id: 16, model : "auto4", age  : 2006, power : 2100, color : "gold"},
-  {id: 94, model : "auto5", age  : 2008, power : 2300, color : "grey"},
-  {id: 74, model : "auto6", age  : 2010, power : 2500, color : "silver"},
-  {id: 4, model : "auto7", age  : 2012, power : 3000, color : "skyblue"},
-  {id: 901, model : "auto8", age  : 2014, power : 4000, color : "#3298f2"},
-  {id: 93, model : "auto9", age  : 2016, power : 21000, color : "#333333"},
-  {id: 2, model : "auto10", age  : 2018, power : 3500, color : "#ff9638"}
-];
+
+const urlSource = (resourse) => `https://jsonplaceholder.typicode.com/${resourse}`
 
 function App() {
 
-  // const [arr,changeArr] = useState(cars);
+  const tabs =[
+  {title: 'posts', clickHHandler: () => setSelectedTab('posts')},
+  {title: 'comments', clickHHandler: () => setSelectedTab('comments')},
+  {title: 'albums', clickHHandler: () => setSelectedTab('albums')},
+  {title: 'photos', clickHHandler: () => setSelectedTab('photos')},
+  {title: 'todos', clickHHandler: () => setSelectedTab('todos')},
+  {title: 'users', clickHHandler: () => setSelectedTab('users')},
+  ]
+  const [selectedTab, setSelectedTab] = useState(tabs[0].title)
+  const [list, setList] = useState([]);
 
-  //   const remove = (itemRemove) => {
-  //   if (itemRemove !== 'first' && itemRemove !== 'last' && itemRemove !== 'revert') return
-  //   const newArr = [...arr];
 
-  //   itemRemove === 'first' && newArr.shift();
-  //   itemRemove === 'last' && newArr.pop();
-  //   changeArr(newArr);
-
-  // }
-
-  //   const onRevert = () => {
-  //     const clone = [...cars];
-  //     changeArr(clone);
-  //   }
-  const [arr,changeArr] = useState(cars);
-
-  const [itemArr,setItem] = useState([]);
-  ;
-
-  const filteredArr = arr.filter(value=> !itemArr.includes(value.id))
-  
-
-  const handleArr = () => {
-    
-    const itemRemove = filteredArr[0];
-    if (!itemRemove) return;
-    setItem([...itemArr, itemRemove.id]);
+  const fetchData = async () => {
+    const response = await fetch(urlSource(selectedTab));
+    const data = await response.json();
+    setList(data);
   }
-
-  const handleArr2 = () => {
-    
-    const itemRemove = filteredArr[filteredArr.length-1];
-    if (!itemRemove) return;
-    setItem([...itemArr, itemRemove.id]);
-
-  }
-
-  const handleArr3 = (value) => {
-    
-
-    if (!value) return;
-    setItem([...itemArr, value.id]);
-  }
-
-  const remove = () => {
-    setItem([]);
-  }
-
+  useEffect(() => {
+    fetchData();
+  }, [selectedTab])
 
   return (
     <div className="App">
-
-      <h3>Cars</h3>
-      <button onClick={handleArr}>Скрыть авто</button>
-      <button onClick={handleArr2}>Скрыть 2 авто</button>
-      <button onClick={remove}>Отобразить все</button>
-      
-      <ul>
-      {
-        filteredArr.map (value => <li key={value.id}><AutoCard {...value}/><button onClick={()=> handleArr3(value)}>Скрыть</button></li>)
-     
-      }
-      </ul>
-
-
-  {/* return (
-    <div className="App">
-
-      <h3>Cars</h3>
-      <button onClick={()=> remove('first')}>Скрыть первое авто</button>
-      <button onClick={()=> remove('last')}>Скрыть последнее авто</button>
-      <button onClick={onRevert}>Отобразить все авто</button>
-      <ul>
-      {
-        arr.map (value => <li key={value.id}><AutoCard {...value}/><button>Delete</button></li>)
-
-      
-      }
-      </ul> */}
-
-      
-    
+      <Tabs tabs={tabs} selectedTab={selectedTab}/>
+      <PostList posts={list}/>
     </div>
   );
 }
