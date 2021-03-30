1f5735cded7b0a7e2338e750a465015149ad13ea
vvaldd <wroztorguev@gmail.com>
2021-03-30 23:01:33 +0300

    1

---
+++ hw2/src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import React, { useState }, { useeffect } from 'react'
+import './App.css';
+
+const Tabs = ({tabs}) => {
+  return (
+
+   <div>
+     {tabs.map}
+   </div> 
+  )
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+function App() {
+  return (
+    <div>
+      <Tabs />
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
