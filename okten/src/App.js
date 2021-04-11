e649f0e0c48323ca59ff4894d6d61c1746362280
vvaldd <wroztorguev@gmail.com>
2021-04-11 15:05:56 +0300

    1

--- okten/src/App.js
+++ okten/src/App.js
@@ -1,15 +1,65 @@
-import React, { useState } from "react";
-import './App.css';
-
-
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App() {
-
     return (
-        <div className="App">
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About!</Link>
+                        </li>
+                        <li>
+                            <Link to="/users">Users</Link>
+                        </li>
+                    </ul>
+                </nav>
 
-        </div>
+                {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+                <Switch>
+                    <Route path="/about">
+                        <About />
+                    </Route>
+                    <Route path="/users">
+                        <Users />
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     );
+}
+
+function Home() {
+    return <h2>Home</h2>;
+}
+
+function About() {
+    return <h2>About</h2>;
+}
+
+function Users() {
+    return <h2>Users</h2>;
+}
+
+function Posts () => {
+    const[posts, setPosts] = useState([]);
+
+    const fetchData = async () => {
+
+    }
 }
 
 export default App;
