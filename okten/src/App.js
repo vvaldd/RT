b65bd06f6f1418dbd10ac0fe4ab524f9ce3e6c7f
vvaldd <wroztorguev@gmail.com>
2021-04-12 23:19:06 +0300

    11

--- okten/src/App.js
+++ okten/src/App.js
@@ -1,14 +1,52 @@
 import React, { useState, createContext, useContext } from "react";
 //import {Switch, Router, Route}
 import './App.css';
+import { Switch, BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
+const TodosList = () => {
+    return (
+        <h3>todos list</h3>
+    )
+}
 
+const AddTodo = () => {
+    return (
+        <h5>add todo</h5>
+    )
+}
+
+const Header = () => {
+    return(
+        <header style={{heigth: '50px', background: 'silver'}}>
+            <Link to='/'>list</Link>
+            <Link to='/create-todo'>add new todo</Link>
+        </header>
+    )
+
+}
 function App() {
 
     return (
-        <div className="App">
-           <h7>Hi</h7>
-        </div>
+        <main>
+
+        <Router>
+
+            <Header />
+
+            <Switch>
+
+                <Route path="/" exact>
+                    <TodosList />
+                </Route>
+
+                <Route path='/create-todo'>
+                    <AddTodo />
+                </Route>
+
+            </Switch>
+        </Router>
+
+        </main>
     );
 }
 
