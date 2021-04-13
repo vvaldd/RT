6171313949dcd86eb6ba258c0140cbb43b93fc68
vvaldd <wroztorguev@gmail.com>
2021-04-13 21:02:55 +0300

    11

--- okten/src/App.js
+++ okten/src/App.js
@@ -3,6 +3,20 @@ import React, { useState, createContext, useContext } from "react";
 import './App.css';
 import { Switch, BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
+const TodoConrext = createContext();
+
+const TodoProvider = ({children}) => {
+
+
+    return (
+        <TodoConrext.Provider>
+            {children}
+
+        </TodoConrext.Provider>
+    )
+}
+
+
 const TodosList = () => {
     return (
         <h3>todos list</h3>
@@ -10,14 +24,33 @@ const TodosList = () => {
 }
 
 const AddTodo = () => {
+    const [todoValues, setTodoValues] = useState({
+        title: '',
+        description: '',
+    })
+
+    const onTodoChange = ({target: { name, value } }) => setTodoValues({ ...todoValues, [name]: value})
+
+    const OnCreate = () => {
+        setTodoValues ({
+            title: '',
+            description: '',
+        })
+    }
     return (
-        <h5>add todo</h5>
+        <div>
+            <input value={todoValues.title} onChange={onTodoChange} type="text" name='title' placeholder="add todo"/>
+            <input value={todoValues.description} onChange={onTodoChange} type="text" name='description' placeholder="todo description"/>
+
+            <button onClick={OnCreate}>add todo</button>
+        </div>
+
     )
 }
 
 const Header = () => {
     return(
-        <header style={{heigth: '50px', background: 'silver'}}>
+        <header>
             <Link to='/'>list</Link>
             <Link to='/create-todo'>add new todo</Link>
         </header>
