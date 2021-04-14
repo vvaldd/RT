45a62dddee780534fc9d8f28eabda83df7217fd2
vvaldd <wroztorguev@gmail.com>
2021-04-14 19:51:37 +0300

    11

--- okten/src/App.js
+++ okten/src/App.js
@@ -1,7 +1,54 @@
-import React, {useState, createContext, useContext} from "react";
+//import React, {useState, createContext, useContext} from "react";
 //import {Switch, Router, Route}
 import './App.css';
-import {Switch, BrowserRouter as Router, Route, Link} from 'react-router-dom'
+//import {Switch, BrowserRouter as Router, Route, Link} from 'react-router-dom'
+
+import {createStore} from 'redux';
+
+
+const initialState = {
+    counter: 0
+}
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INC': {
+            return {
+                ...state,
+                counter: state.counter + 1
+            }
+        }
+
+        case 'DEC': {
+            return {
+                ...state,
+                counter: state.counter - 1
+            }
+        }
+        case 'RESET': {
+            return {
+                ...state,
+                counter: 0
+            }
+        }
+
+        default:
+            console.log('action')
+            return state;
+
+    }
+}
+const store = createStore(reducer)
+
+console.log(store);
+
+store.subscribe(() => {
+    console.log('tut', store.getState());
+})
+
+store.dispatch({type: 'INC'})
+store.dispatch({type: 'INC'})
+store.dispatch({type: 'INC'})
+
 
 function App() {
 
