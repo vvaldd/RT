59ec17a99404d0e03a76a88bf0b3582f03b9827d
vvaldd <wroztorguev@gmail.com>
2021-04-26 19:06:08 +0300

    1

--- okten/src/App.js
+++ okten/src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 
 
 function App() {
+    const { counter, setCounter } = useState(0);
+    const inc = () => {
+        setCounter(counter + 1);
+    }
 
-  return (
-    <div className="App">
-      
-        <h3>Hi</h3>
-      
-    </div>
-  );
+    return (
+        <div className="App">
+
+            <button style={{weidth: '20px'}}>{counter}</button>
+
+        </div>
+    );
 }
 
 export default App;
