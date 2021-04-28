b9c7e99d661b9ee4e089b537dcd79ef32a36e6b4
vvaldd <wroztorguev@gmail.com>
2021-04-28 19:17:51 +0300

    1

--- okten/src/App.js
+++ okten/src/App.js
@@ -3,15 +3,32 @@ import './App.css';
 
 
 function App() {
-    const { counter, setCounter } = useState(0);
+    const [counter, setCounter] = useState(0);
+    const [counter1, setCounter1] = useState(1);
     const inc = () => {
-        setCounter(counter + 1);
+        setCounter(prev => prev + 1); /*useState callback*/
+        console.log(counter);
     }
+    const inc1 = () => {
+        setCounter1(prev => prev + 1); /*useState callback*/
+        console.log(counter1);
+    }
+    // useEffect (() => {
+    //     console.log(counter);
+    // }, [counter])
+
+    const complexLogic = useMemo(() => {    /*useMemo*/
+        return fn(4, counter1)
+    }, [counter1]);
+
+    console.log(complexLogic);
 
     return (
         <div className="App">
+            
 
-            <button style={{weidth: '20px'}}>{counter}</button>
+            <button onClick={inc}>{counter}</button>
+            <button onClick={inc1}>{counter1}</button>
 
         </div>
     );
